b126092cded846b7bb79540469475e58917fe26b
Marcelo Silva <mlsilva.marcelo@gmail.com>
2022-04-16 21:42:33 -0300

    feat: Add apollo client configuration
    Consume first query

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,14 @@
+import { ApolloProvider } from '@apollo/client';
 import { UserProvider } from '@auth0/nextjs-auth0';
+import { apolloClient } from '../lib/apollo';
 
 function MyApp({ Component, pageProps }) {
   return (
     <UserProvider>
-      <Component {...pageProps} />
-    </UserProvider>
+      <ApolloProvider client={apolloClient}>
+        <Component {...pageProps} />
+      </ApolloProvider>
+    </UserProvider >
   )
 }
 
--- src/pages/app/index.tsx
+++ src/pages/app/index.tsx
@@ -1,13 +1,26 @@
+import { gql, useQuery } from "@apollo/client";
 import { getSession, useUser, withPageAuthRequired } from "@auth0/nextjs-auth0";
 import { GetServerSideProps } from "next";
 
+const PRODUCTS_QUERY = gql`
+  query GetProducts {
+    products {
+      id 
+      title
+    }
+  }
+`
+
+
 export default function Home() {
   const { user } = useUser();
-
+  const { data, loading, error } = useQuery(PRODUCTS_QUERY);
   return (
     <div>
       <h1>Bem vindo</h1>
-
+      <pre>
+        {JSON.stringify(data, null, 2)}
+      </pre>
       <pre>
         {JSON.stringify(user, null, 4)}
       </pre>
@@ -17,7 +30,7 @@ export default function Home() {
 
 //export const getServerSideProps = withPageAuthRequired();
 
-export const getServerSideProps: GetServerSideProps = async ({req, res}) => {
+export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
   const session = getSession(req, res);
 
   if (!session) {
