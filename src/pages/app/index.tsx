1fc41a18fa09e7fd1b53113cc9570abefb75db2e
Marcelo Silva <mlsilva.marcelo@gmail.com>
2022-04-28 11:19:01 -0300

    feat: Get access token to obtain auth data

--- src/pages/app/index.tsx
+++ src/pages/app/index.tsx
@@ -1,15 +1,20 @@
 import { useUser, withPageAuthRequired } from "@auth0/nextjs-auth0";
+import { useMeQuery } from "../../graphql/generated/graphql";
 import { getServerPageGetProducts, ssrGetProducts } from "../../graphql/generated/page";
 import { withApollo } from "../../lib/withApollo";
 
 export function Home({ data }) {
   const { user } = useUser();
+  const { data: me } = useMeQuery();
   return (
     <div>
       <h1>Bem vindo</h1>
       <pre>
-        {JSON.stringify(data.products, null, 2)}
+        ME: {JSON.stringify(me, null, 2)}
       </pre>
+      {/* <pre>
+        {JSON.stringify(data.products, null, 2)}
+      </pre> */}
       <pre>
         {JSON.stringify(user, null, 4)}
       </pre>
@@ -19,7 +24,10 @@ export function Home({ data }) {
 
 export const getServerSideProps = withPageAuthRequired({
   getServerSideProps: async (ctx) => {
-    return await getServerPageGetProducts(null, ctx);
+    //return await getServerPageGetProducts(null, ctx);
+    return {
+      props: {}
+    }
   }
 });
 
